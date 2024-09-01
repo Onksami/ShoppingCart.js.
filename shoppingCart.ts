2cdfac5c880dc328ed3f44dfd1b67e45666e20e9
Sami Onk <onksami@gmail.com>
2024-09-01 03:43:48 +0200

    refactor: converting from js to ts is ok.

---
+++ shoppingCart.ts
@@ -0,0 +1,53 @@
+// Step 1: Define a type for Categories
+type Category =  number | string ;
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+  category: Category;
+}
+
+let shoppingCart: Product[] = [];
+
+
+const Categories: { [key in Category]: Category } = {
+  ELECTRONICS: "Electronics",
+  GROCERIES: "Groceries",
+  CLOTHING: "Clothing",
+};
+
+const products: Product[] = [
+  { id: 1, name: "Laptop", price: 999.99, category: Categories.ELECTRONICS },
+  { id: 2, name: "T-Shirt", price: 19.99, category: Categories.CLOTHING },
+  { id: 3, name: "Bananas", price: 1.99, category: Categories.GROCERIES },
+];
+
+
+
+const addToCart = (product: Product): void => {
+  shoppingCart.push(product);
+};
+
+
+const calculateTotal = (cart: Product[]): string => {
+  let total = 0;
+  cart.forEach((item) => (total += item.price));
+  return total.toFixed(2);
+};
+
+const displayCart = (cart: Product[]): void => {
+  if (cart.length === 0) {
+    return;
+  }
+
+  console.log("Your cart contains:");
+  cart.forEach((item) => {
+    console.log(`- ${item.name} (${item.category}): $${item.price}`);
+  });
+  console.log(`Total: $${calculateTotal(cart)}`);
+};
+
+addToCart(products[0]); // Adding Laptop
+addToCart(products[2]); // Adding Bananas
+displayCart(shoppingCart);
